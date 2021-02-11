949f95d7379354f2938f8856a5726cfa4af513d4
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-11 13:53:59 -0600

    Removed gestures class. Possible addition after MVP. Added class notes to Notes class for clarity

--- RPSLS/Notes.cs
+++ RPSLS/Notes.cs
@@ -18,15 +18,36 @@ namespace RPSLS
         //         -randomly choose AI gesture
         //         -compare gestures against a list to determine winner
         //         -Display result and winner for round
+        //         -Increment points
         //Repeat game for 3 rounds
         //Determine winner and display message on screen
         //Either conclude or loop back to Main Menu(Exit option needed on loop)
 
         //Game class
         //Player class. ---Methods can be overriden for players or AI to distinguish---
-        //Gestures may need to be class with inheritance
+        //Gestures may need to be class with inheritance(maybe later)
         //Program class runs scenario
 
-
+        //GAMEFLOW: PLAIN ENGLISH
+        //PROBLEM: HOW DO WE PLAY THIS GAME?!?!?!
+        //●Welcome to the game
+        //●Explain the rules 
+        //●Select players - is this single player or multiplayer(human vs. human OR human vs. computer)
+        //●Choose names(two player, each chooses name, single player, name your opponent)
+        //●Select the number of rounds(best of)
+        //●Display the gesture options
+        //●Both players choose their gestures
+        //●Compare the gestures!●Determine which gesture wins
+        //●Track the score of who won the round!
+        //●Check to see if either player has enough points to win
+        //●If neither player has the winning score, repeat until someone does!
+        //●Declare the winner!
+        //●End the game or play again!MVP = MINIMUM VIABLE PRODUCT - get your program working first, then make it better! 
+        
+        //CLASSES NEEDED:
+        //●Game class
+        //●Player(PARENT)
+        //●Human(CHILD OF Player)
+        //●AI or Computer class (CHILD of Player)
     }
 }
