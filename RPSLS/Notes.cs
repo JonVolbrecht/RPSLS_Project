7cbc7d28420a52809c728a24825168ba294977b2
JonVolbrecht <jonathanvolbrecht@yahoo.com>
2021-02-11 10:31:09 -0600

    Notes class created to track progress and change depending on scenario

---
+++ RPSLS/Notes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    class Notes
+    {
+        //Hey! Want to play RSPLS? -Initiate game/welcome to game
+        //Main Menu -Choose Single player or multiplayer
+        //Input number of AI(limit to two) or players
+        //Choose number of rounds(minimum 3)
+        //Run game -choose player gesture
+        //         -randomly choose AI gesture
+        //         -compre gestures against a method(s) to determine winner
+        //         -Display result
+        //Repeat game for 3 rounds
+        //Determine winner and display message on screen
+        //
+        
+        
+        
+    }
+}
